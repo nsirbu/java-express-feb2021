27e5084a3b58b8835abf61b0562db381ccee85d7
Nicolae Sirbu <nsirbu@inthergroup.com>
2021-02-02 09:36:46 +0200

    Created the first Java project.

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,15 @@
+//*************************************************//
+//          INTHER LOGISTICS ENGINEERING           //
+//*************************************************//
+
+/**
+ * @author nsirbu
+ * @since 01.02.2021
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+
+}
---
+++ FirstJavaProject/src/edu/tekwill/vehicles/Car.java
@@ -0,0 +1,16 @@
+//*************************************************//
+//          INTHER LOGISTICS ENGINEERING           //
+//*************************************************//
+
+package edu.tekwill.vehicles;
+
+/**
+ *
+ */
+public class Car {
+
+  /*
+   dkfj
+   */
+  int a = 0;
+}
